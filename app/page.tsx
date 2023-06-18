9b7b50266b841ed3479cb5e5cc2bdfb2d94b5097
allarounddev <allarounddeveloper@outlook.com>
2023-06-18 18:34:54 +0900

    AI weather app

---
+++ app/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import CityPicker from "@/components/CityPicker";
+import { Card, Divider, Subtitle, Text } from "@tremor/react";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-[#394F68] to-[#183B7E] p-10 flex flex-col justify-center items-center">
+      <Card className="max-w-4xl mx-auto">
+        <Text className="text-6xl font-bold text-center mb-10">Weather AI</Text>
+        <Subtitle className="text-xl text-center">
+          Powered by OpenAI, Next.js 13.3, Tailwind CSS, Tremor 2.0 + More!
+        </Subtitle>
+
+        <Divider className="my-10" />
+
+        <Card className="bg-gradient-to-br from-[#394F68] to-[#183B7E]">
+          <CityPicker />
+        </Card>
+      </Card>
+    </div>
+  );
+}
---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["www.weatherbit.io"],
+  },
+  experimental: {
+    appDir: true,
+    serverComponentsExternalPackages: ["@tremor/react"],
+  },
+};
+
+module.exports = nextConfig;
